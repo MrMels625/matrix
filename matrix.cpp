10e29f6c55b6b0524acb9c4a3487b348d7d94594
MrMels625 <MrMels625@gmail.com>
2024-10-18 19:50:51 +0300

    Added function definitions for matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,14 +1,53 @@
 #include <iostream>
 #include "matrix.hpp"
 
-void destroyMatrix(int** matrix, size_t m)
-{}
+void destroyMatrix(int** matrix, size_t n)
+{
+  for (size_t i = 0; i < m; i++)
+  {
+    delete[] matrix[i];
+  }
+  delete[] matrix;
+}
 
 int** createMatrix(size_t m, size_t n)
-{}
+{
+  int** matrix = new int* [m];
+  size_t created = 0;
+  try
+  {
+    for (; created < m; created++)
+    {
+      matrix[created] = new int[n];
+    }
+  }
+  catch (const std::bad_alloc & e) {
+    destroyMatrix(matrix, created);
+    throw;
+  }
+  return matrix;
+}
 
 void readMatrix(int** matrix, size_t m, size_t n)
-{}
+{
+  for (size_t i = 0; i < m; i++)
+  {
+    for (size_t j = 0; j < n; j++)
+    {
+      std::cin >> matrix[i][j];
+    }
+  }
+}
 
 void writeMatrix(const int* const* matrix, size_t m, size_t n)
-{}
+{
+  for (size_t i = 0; i < m; i++)
+  {
+    std::cout << matrix[i][0];
+    for (size_t j = 1; j < n; j++)
+    {
+      std::cout << " " << matrix[i][j];
+    }
+    std::cout << "\n";
+  }
+}
