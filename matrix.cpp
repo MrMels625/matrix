020e1fc9c6ebd0da0d8f6f8d84ee9ec9d8c5ebc8
MrMels625 <MrMels625@gmail.com>
2024-10-18 19:44:30 +0300

    Added matrix.cpp

---
+++ matrix.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "matrix.hpp"
+
+void destroyMatrix(int** matrix, size_t m)
+{}
+
+int** createMatrix(size_t m, size_t n)
+{}
+
+void readMatrix(int** matrix, size_t m, size_t n)
+{}
+
+void writeMatrix(const int* const* matrix, size_t m, size_t n)
+{}
