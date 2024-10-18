8c2bddc0d20916431b9ca4ae987ceaabb8bcdb8e
MrMels625 <MrMels625@gmail.com>
2024-10-18 19:55:28 +0300

    Added function calls in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include "matrix.hpp"
 
 int main()
 {
   size_t M = 0, N = 0;
   std::cin >> M >> N;
+
+  int** matrix = nullptr;
+  try
+  {
+    matrix = createMatrix(M, N);
+  } catch (const std::bad_alloc & e) {
+    std::cerr << "ERROR: Out of memory!\n";
+    destroyMatrix(matrix, M);
+    return 1;
+  }
+  readMatrix(matrix, M, N);
+  writeMatrix(matrix, M, N);
+  destroyMatrix(matrix, M);
 }
